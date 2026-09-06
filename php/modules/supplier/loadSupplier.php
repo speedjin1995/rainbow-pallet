ab58cc101be61a42957f6ae41a2b497c008a088a
Ng Jih Bin <sky.ng@dcap.my>
2026-09-06 23:31:18 +0800

    bug fix on payment term

--- php/modules/supplier/loadSupplier.php
+++ php/modules/supplier/loadSupplier.php
@@ -35,20 +35,23 @@ $data = array();
 
 while ($row = mysqli_fetch_assoc($empRecords)) {
     $data[] = array(
-        "id"             => $row['id'],
-        "supplier_code"  => $row['supplier_code'],
-        "name"           => $row['name'],
+        "id" => $row['id'],
+        "supplier_code" => $row['supplier_code'],
+        "name" => $row['name'],
         "company_reg_no" => $row['company_reg_no'],
-        "new_reg_no"     => $row['new_reg_no'],
+        "new_reg_no" => $row['new_reg_no'],
         "address_line_1" => $row['address_line_1'],
         "address_line_2" => $row['address_line_2'],
         "address_line_3" => $row['address_line_3'],
-        "phone_no"       => $row['phone_no'],
-        "fax_no"         => $row['fax_no'],
-        "contact_name"   => $row['contact_name'],
-        "ic_no"          => $row['ic_no'],
-        "tin_no"         => $row['tin_no'],
-        "status"         => $row['status']
+        "phone_no" => $row['phone_no'],
+        "fax_no" => $row['fax_no'],
+        "contact_name" => $row['contact_name'],
+        "ic_no" => $row['ic_no'],
+        "tin_no" => $row['tin_no'],
+        "payment_term" => $row['payment_term'],
+        "payment_term_period" => $row['payment_term_period'],
+        "account_no" => $row['account_no'],
+        "status" => $row['status']
     );
 }
 
